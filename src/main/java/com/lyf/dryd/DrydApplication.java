72970b99af3de4d1cb795cb054e87fd2ef35fd58
hana <jdxtxbb@163.com>
2020-05-01 15:46:52 +0800

    初始化

---
+++ src/main/java/com/lyf/dryd/DrydApplication.java
@@ -0,0 +1,13 @@
+package com.lyf.dryd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrydApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyf/dryd/DrydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyf.dryd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
